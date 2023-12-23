8127835cce4210f0a6fc0dcb0638e359bdf5f17c
NwobiaDavid <dnwobia@gmail.com>
2023-12-23 20:49:48 +0100

    added a copy to clipboard feature and renamed some of the variables

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
 from tkinter import *
 import pyshorteners
-
-window = Tk()
-window.title('URL Shortener')
-icon = PhotoImage(file='C:\\Users\\Nwobia David\\Downloads\\XAES.png')
-window.iconphoto(True, icon)
-window.geometry('500x800')
+from tkinter import messagebox
 
 def shorten():
     if entry2.get():
         entry2.delete(0, END)
 
     if entry.get():
-        url = pyshorteners.Shortener().tinyurl.short(entry.get())
-        entry2.insert(END, url)
+        try:
+            url = pyshorteners.Shortener().tinyurl.short(entry.get())
+            entry2.insert(END, url)
+            print(pyshorteners.Shortener().tinyurl.expand(url))
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred: {e}")
 
-        print(pyshorteners.Shortener().tinyurl.expand(url))
+def copy_to_clipboard():
+    if entry2.get():
+        window.clipboard_clear()
+        window.clipboard_append(entry2.get())
+        window.update()
+        messagebox.showinfo("Success", "Shortened link copied to clipboard!")
 
-label = Label(window, text='enter the link to shorten', font=('poppins', 28))
-label.pack(pady=20)
+# Set up the main window
+window = Tk()
+window.title('URL Shortener')
+icon = PhotoImage(file='C:\\Users\\Nwobia David\\Downloads\\XAES.png')
+window.iconphoto(True, icon)
+window.geometry('500x400')
 
-entry = Entry(window, font=('poppins', 22))
-entry.pack(pady=20)
+# Create and pack widgets
+label = Label(window, text='Enter the link to shorten', font=('poppins', 18))
+label.pack(pady=10)
 
-btn = Button(window, text='shorten link', command=shorten, font=('poppins', 24))
-btn.pack(pady=20)
+entry = Entry(window, font=('poppins', 14))
+entry.pack(pady=10)
 
-label2 = Label(window, text='shortened link', font=('Poppins', 20))
-label2.pack(pady=50)
+btn_shorten = Button(window, text='Shorten Link', command=shorten, font=('poppins', 16))
+btn_shorten.pack(pady=10)
 
-entry2 = Entry(window, font=('poppins', 22), justify=CENTER, width=30, bd=0, bg='systembuttonface' )
+label2 = Label(window, text='Shortened Link', font=('Poppins', 16))
+label2.pack(pady=20)
+
+entry2 = Entry(window, font=('poppins', 14), justify=CENTER, width=30, bd=0, bg='systembuttonface')
 entry2.pack(pady=10)
 
+btn_copy = Button(window, text='Copy to Clipboard', command=copy_to_clipboard, font=('poppins', 14))
+btn_copy.pack(pady=10)
 
 window.mainloop()
